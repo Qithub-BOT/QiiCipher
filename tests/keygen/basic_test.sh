eef0c5c5a2950a097be67ce794a4013454b52871
KEINOS <github@keinos.com>
2021-05-29 13:45:07 +0900

    feat: test:「引数なし」時の動作テスト
    各コマンドが「引数なし」で呼ばれた場合に、ヘルプの表示とステータス 1  で終了するかのテスト。

---
+++ tests/keygen/basic_test.sh
@@ -0,0 +1,9 @@
+#shellcheck shell=sh
+
+Describe 'keygen with no arg'
+    It 'should print help with status 1'
+        When call keygen
+        The output should include "使い方"
+        The status should be failure # status is 1-255
+    End
+End
