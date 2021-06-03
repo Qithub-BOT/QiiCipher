5bcd381cb5838c1ac33acde06b6c026daf719eb6
sato yoshiyuki <yoshi.389111@gmail.com>
2021-06-03 09:01:16 +0000

    add arg_normal_test.sh for check

---
+++ tests/check/arg_normal_test.sh
@@ -0,0 +1,11 @@
+Describe 'check with args'
+    Mock curl
+        cat "${PATH_DIR_TEST}/.ssh/openssh/no_pass/id_rsa.pub"
+    End
+    It 'should print "no problem" with status 0'
+        When call "${PATH_DIR_BIN}/check" Qithub-BOT "${PATH_DIR_TEST}/.ssh/openssh/no_pass/id_rsa.pem"
+
+        The output should include "問題なさそうです。"
+        The status should be success
+    End
+End
