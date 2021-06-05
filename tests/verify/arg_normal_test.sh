7b5b03996dfe32aa99869dff055cc1b2b128c9fd
sato yoshiyuki <yoshi.389111@gmail.com>
2021-06-06 00:48:26 +0900

    add verify/arg_normal_test.sh

---
+++ tests/verify/arg_normal_test.sh
@@ -0,0 +1,13 @@
+#shellcheck shell=sh
+
+Describe 'verify with args'
+    Mock curl
+        cat "${PATH_DIR_TEST}/.ssh/openssh/no_pass/id_rsa.pub"
+    End
+    It 'should print "no problem" with status 0'
+        When call "${PATH_DIR_BIN}/verify" "${PATH_DIR_TEST}/.ssh/openssh/test_data/private_data" Qithub-BOT "${PATH_DIR_TEST}/.ssh/openssh/test_data/private_data.sig"
+
+        The output should include "このファイルは正しく署名されています。"
+        The status should be successful
+    End
+End
