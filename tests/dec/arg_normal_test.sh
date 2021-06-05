29115485d2acc103c10bacad774c94fd42799035
sato yoshiyuki <yoshi.389111@gmail.com>
2021-06-06 00:20:27 +0900

    add dec/arg_normal_test.sh

---
+++ tests/dec/arg_normal_test.sh
@@ -0,0 +1,28 @@
+#shellcheck shell=sh
+
+file_compare() {
+    cmp -s \
+        "${PATH_DIR_WORK}/private_data.dec" \
+        "${PATH_DIR_TEST}/.ssh/openssh/test_data/private_data" \
+        && echo "successful" || echo "failure"
+}
+
+cleanup() {
+    rm -f "${PATH_DIR_WORK}/private_data.*"
+}
+
+Describe 'dec with args'
+    After "cleanup"
+
+    It 'should print key length with status 0'
+        When call "${PATH_DIR_BIN}/dec" "${PATH_DIR_TEST}/.ssh/openssh/no_pass/id_rsa.pem" "${PATH_DIR_TEST}/.ssh/openssh/test_data/private_data.enc" "${PATH_DIR_WORK}/private_data.dec"
+
+        The output should include "ファイルを復号しました。"
+        The status should be successful
+
+        Path file_enc="${PATH_DIR_WORK}/private_data.dec"
+        The path file_enc should be exist
+
+        The result of "file_compare()" should equal "successful"
+    End
+End
