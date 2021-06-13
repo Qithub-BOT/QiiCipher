b5daccd2e5b3284ff648b0f21a674eea9bc39856
sato yoshiyuki <yoshi.389111@gmail.com>
2021-06-13 10:45:52 +0900

    change message improvement

--- tests/issues/issue34_test.sh
+++ tests/issues/issue34_test.sh
@@ -7,8 +7,7 @@ Describe 'sign with unexisting key'
     It 'should print err with status 1 and should not create .enc file'
         When call "${PATH_DIR_BIN}"/sign KEINOS '/path/to/unknown/key.pub' "$name_file_to_sign" "$path_file_sig_out"
 
-        The stdout should include '署名ファイルを生成できませんでした'
-        The stderr should include 'No such file or directory'
+        The stderr should include '秘密鍵ファイル /path/to/unknown/key.pub が見つかりません。'
         The status should be failure # status is 1-255
 
         Path file_sig="$path_file_sig_out"
